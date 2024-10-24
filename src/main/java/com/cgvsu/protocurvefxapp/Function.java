5e8304c7f3b5aa184766628fc6c0d42b1e14aac3
Egor <zuzik17@yandex.ru>
2024-10-24 14:30:31 +0300

    Добавил в список первую и последнюю точки (а, б) поправил рисовку, на простых данных проект работает.

--- src/main/java/com/cgvsu/protocurvefxapp/Function.java
+++ src/main/java/com/cgvsu/protocurvefxapp/Function.java
@@ -10,46 +10,45 @@ public class Function {
 
     List<Point2D> list;
     List<Point2D> rez = new ArrayList<>();
-    int n ;
+    int n;
 
     public List<Point2D> getRez() {
         return rez;
     }
 
     public Function(List<Point2D> list) {
-        Comparator<Point2D> comparator = new Comparator<Point2D>() {
-            @Override
-            public int compare(Point2D o1, Point2D o2) {
-                if (o1.getX() == o2.getX()) return 0;
-                return o1.getX() > o2.getX() ? 1 : -1;
-            }
-        };
-        list.sort(comparator);
         this.list = list;
-        this.n= list.size();
+        this.n = list.size();
         function();
-//        if (list.getFirst().getX() != (int) list.getFirst().getX()) rez.add(list.getFirst());
     }
 
     private void function() {
+        Comparator<Point2D> comparator = (o1, o2) -> {
+            if (o1.getX() == o2.getX()) return 0;
+            return o1.getX() > o2.getX() ? 1 : -1;
+        };
+        list.sort(comparator);
+
+        if (list.get(0).getX() != (int) list.get(0).getX()) rez.add(list.get(0));
         int a = (int) list.get(0).getX() + 1;
-        int b = (int) list.get(n-1).getX();
+        int b = (int) list.get(n - 1).getX();
         for (; a <= b; a++) {
             rez.add(Polinom(a));
         }
-        if (list.get(n-1).getX()!=(int)list.get(n-1).getX()) rez.add(list.get(n-1));
+        if (list.get(n - 1).getX() != (int) list.get(n - 1).getX()) rez.add(list.get(n - 1));
     }
 
-    private  Point2D Polinom(int x) {
-        double y=0;
-        for (int i = 0; i <n; i++) {
-            double l =list.get(i).getY();
+    private Point2D Polinom(int x) {
+        double y = 0;
+        for (int i = 0; i < n; i++) {
+            double l = list.get(i).getY();
             for (int j = 0; j < n; j++) {
-                if(i!=j){
-                    l=l*(x-list.get(j).getX())/(list.get(i).getX()-list.get(j).getX());
+                if (i != j) {
+                    l = l * (x - list.get(j).getX()) / (list.get(i).getX() - list.get(j).getX());
                 }
             }
+            y += l;
         }
-        return new Point2D(x,y);
+        return new Point2D(x, y);
     }
 }
